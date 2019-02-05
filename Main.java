a60a8f9dbf89ae47fd84df2fbb801f46f6c356dd
Davis Perchik <davisperchik@zipcoders-MacBook-Pro.local>
2019-02-05 11:49:25 -0500

    Able to say hello to either Alice or Bob once

--- Main.java
+++ Main.java
@@ -1,9 +1,15 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
-
-    public static void main(String[] args ){
-
+    
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String n = reader.nextLine();
+        if (n.equals("Alice") || n.equals("Bob")) {
+            System.out.println("Hello");
+        }
     }
 }
